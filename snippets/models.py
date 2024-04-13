6e423eefb395df34e1fd52c8324f06b79817714f
Mficius <mayisficius@gmail.com>
2024-04-13 16:02:30 +0200

    last commit

--- snippets/models.py
+++ snippets/models.py
@@ -7,11 +7,11 @@ LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 # Create your models here.
 
-class snippets(models.Model):
+class Snippets(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
-    linennos = models.BooleanField(default=False)
+    linenos = models.BooleanField(default=False)
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
     style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
 
